6d567d861d8eda8b3eedf560874f5d80be4e10d4
Julio Maia <jmaia@amd.com>
2023-10-12 14:38:01 -0700

    Few improvements to build_gmx.sh

--- build_gmx.sh
+++ build_gmx.sh
@@ -6,12 +6,11 @@ if [ ! -d ./Gromacs ]; then
   mkdir Gromacs/build-threads && cd Gromacs/build-threads
 
 
-  # runs cmake on GROMACS to produce a tmpi build of gromacs
-
-  cmake ../ -DBUILD_SHARED_LIBS=off \
+    # runs cmake on GROMACS to produce a tmpi build of gromacs
+  echo "cmake ../ -DBUILD_SHARED_LIBS=off \
    -DCMAKE_BUILD_TYPE=Release \
-   -DCMAKE_C_FLAGS="-Ofast -ggdb -I${ROCM_PATH}/roctracer/include" \
-   -DCMAKE_CXX_FLAGS="-Ofast -ggdb -I${ROCM_PATH}/roctracer/include" \
+   -DCMAKE_C_FLAGS=\"-Ofast -ggdb -I${ROCM_PATH}/roctracer/include\" \
+   -DCMAKE_CXX_FLAGS=\"-Ofast -ggdb -I${ROCM_PATH}/roctracer/include\" \
    -DGMX_BUILD_OWN_FFTW=ON \
    -DGMX_BUILD_FOR_COVERAGE=off \
    -DCMAKE_C_COMPILER=gcc \
@@ -24,11 +23,14 @@ if [ ! -d ./Gromacs ]; then
    -DBUILD_TESTING=off \
    -DGMXBUILD_UNITTESTS=off \
    -DGMX_GPU_USE_VKFFT=on \
-   -DHIP_HIPCC_FLAGS="-O3 -ggdb --offload-arch=$ARCH_BUILDS --save-temps -I${MPICH_DIR}/include -I${ROCM_PATH}/roctracer/include" \
-   -DCMAKE_EXE_LINKER_FLAGS="-L${ROCM_PATH}/lib -L${ROCM_PATH}/roctracer/lib -fopenmp"
+   -DHIP_HIPCC_FLAGS=\"-O3 -ggdb --offload-arch=$ARCH_BUILDS --save-temps -I${MPICH_DIR}/include -I${ROCM_PATH}/roctracer/include\" \
+   -DCMAKE_EXE_LINKER_FLAGS=\"-L${ROCM_PATH}/lib -L${ROCM_PATH}/roctracer/lib -fopenmp\"
 
-    make -j
-    cd ../../
+    make -j" > build_gmx_threads.sh
+  chmod u+x build_gmx_threads.sh
+	./build_gmx_threads.sh
+	wait
+  cd ../../
 fi
 
 # Extracts the stmv topology if we didn't do that already
